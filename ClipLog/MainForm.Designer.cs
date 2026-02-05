498e61f55fcea0710dd37ff390577404d2ac67e0
T800 <25018911+T800G@users.noreply.github.com>
2026-02-05 21:41:54 +0100

    Add files via upload

---
+++ ClipLog/MainForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: T800
+ * Date: 19.11.2023.
+ * Time: 13:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ClipLog
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(299, 70);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ClipLog";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.ResumeLayout(false);
+		}
+	}
+}
---
+++ ClipLog/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+
+namespace ClipLog
+{
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// Places the given window in the system-maintained clipboard format listener list.
+		/// </summary>
+		[DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+	    [return: MarshalAs(UnmanagedType.Bool)]
+	    private static extern bool AddClipboardFormatListener(IntPtr hwnd);
+		 
+		/// <summary>
+		/// Removes the given window from the system-maintained clipboard format listener list.
+		/// </summary>
+	    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+	    [return: MarshalAs(UnmanagedType.Bool)]
+	    private static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+		 
+		/// <summary>
+		/// Sent when the contents of the clipboard have changed.
+		/// </summary>
+		private const int WM_CLIPBOARDUPDATE = 0x031D;
+		
+		private System.IO.StreamWriter m_logwriter;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			// init file logging
+			m_logwriter = new System.IO.StreamWriter("ClipLog - " + DateTime.Now.ToString("yyyy.MM.dd - ") + DateTime.Now.ToString("HH.mm.ss") + ".txt", true, Encoding.Unicode);
+			this.FormClosing += OnFormClosing;
+			bool b = AddClipboardFormatListener(this.Handle);
+		}
+
+		
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+	 
+	        if (m.Msg == WM_CLIPBOARDUPDATE)
+	        {
+	            IDataObject iData = Clipboard.GetDataObject();
+	            if (iData.GetDataPresent(DataFormats.StringFormat))
+	            {
+	                string cliptext = iData.GetData(DataFormats.StringFormat).ToString();
+	                m_logwriter.WriteLine(cliptext);
+	                m_logwriter.Flush();
+	            }
+	        }			
+		}
+
+		
+		private void OnFormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			bool b = RemoveClipboardFormatListener(this.Handle);
+			m_logwriter.Close();
+		}
+		
+	}
+}
